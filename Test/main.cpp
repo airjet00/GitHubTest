762952f66c8ad797f55778a2a775b1f0d44cd495
erikskell <erikskell@gmail.com>
2022-01-18 09:19:50 -0900

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Testing XCode and XCode link w/
+//  GitHub/Git. Will keep in Repo for
+//  future test. 
+//
+//  Created by Erik Kell on 1/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
