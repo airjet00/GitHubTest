5a2951c0d2b75b719788cc3c5cdbd11a27c5a9e7
erikskell <erikskell@gmail.com>
2022-01-20 08:28:45 -0900

    Created main (Hello, GitHub) test

---
+++ GitHubTest/GitHubTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitHubTest
+//
+//  Created by Erik Kell on 1/18/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, GitHub!\n";
+    return 0;
+}
